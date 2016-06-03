b4f0735dd002ab9adf284d6263d28d8163f1b4b9
Bernhard <bernhard.walter@me.com>
2016-06-03 10:38:53 +0200

    Initial Commit

---
+++ dumpPhotos/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dumpPhotos
+//
+//  Created by Bernhard Walter on 03.06.16.
+//  Copyright © 2016 Bernhard Walter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
